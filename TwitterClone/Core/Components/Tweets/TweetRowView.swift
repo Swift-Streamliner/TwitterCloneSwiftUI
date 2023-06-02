132f46bdf3fb295caaf456e98777c567786daa76
tiger <thisisstreamliner@gmail.com>
2023-06-02 08:19:14 +0300

    Add Feed UI

---
+++ TwitterClone/Core/Components/Tweets/TweetRowView.swift
@@ -0,0 +1,85 @@
+//
+//  TweetRowView.swift
+//  TwitterClone
+//
+//  Created by MEI on 01.06.2023.
+//
+
+import SwiftUI
+
+struct TweetRowView: View {
+    var body: some View {
+        VStack (alignment: .leading) {
+            // profile image + user info + tweet
+            HStack (alignment: .top, spacing: 12) {
+                Circle()
+                    .frame(width: 56, height: 56)
+                    .foregroundColor(Color(.systemBlue))
+                
+                // User info and tweet caption
+                VStack (alignment: .leading, spacing: 4){
+                    HStack {
+                        Text("Bruce Wayne")
+                            .font(.subheadline).bold()
+                        Text("@batman")
+                            .foregroundColor(.gray)
+                            .font(.caption)
+                        Text("2w")
+                            .foregroundColor(.gray)
+                            .font(.caption)
+                    }
+                    
+                    Text("I believe in Harvey Dent")
+                        .font(.subheadline)
+                        .multilineTextAlignment(.leading)
+                }
+            }
+            // action buttons
+            HStack {
+                Button {
+                    
+                } label: {
+                    Image(systemName: "bubble.left")
+                        .font(.subheadline)
+                }
+                
+                Spacer()
+                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "arrow.2.squarepath")
+                        .font(.subheadline)
+                }
+                
+                Spacer()
+                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "heart")
+                        .font(.subheadline)
+                }
+                
+                Spacer()
+                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "bookmark")
+                        .font(.subheadline)
+                }
+            }
+            .padding()
+            .foregroundColor(.gray)
+            
+            Divider()
+        }
+    }
+}
+
+struct TweetRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        TweetRowView()
+    }
+}
