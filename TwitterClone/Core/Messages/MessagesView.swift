c0ea1802562488ec080178a540a69dd11b4e6303
tiger <thisisstreamliner@gmail.com>
2023-06-07 20:39:27 +0300

    MainTabView

---
+++ TwitterClone/Core/Messages/MessagesView.swift
@@ -0,0 +1,20 @@
+//
+//  MessagesView.swift
+//  TwitterClone
+//
+//  Created by MEI on 07.06.2023.
+//
+
+import SwiftUI
+
+struct MessagesView: View {
+    var body: some View {
+        Text("Messages View")
+    }
+}
+
+struct MessagesView_Previews: PreviewProvider {
+    static var previews: some View {
+        MessagesView()
+    }
+}
