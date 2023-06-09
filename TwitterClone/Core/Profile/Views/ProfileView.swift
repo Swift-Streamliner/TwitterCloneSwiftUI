127a329fb066fe9843a302dbfed3dd2d72c006fd
tiger <thisisstreamliner@gmail.com>
2023-06-09 20:22:34 +0300

    Create ActionButtons

--- TwitterClone/Core/Profile/Views/ProfileView.swift
+++ TwitterClone/Core/Profile/Views/ProfileView.swift
@@ -11,6 +11,7 @@ struct ProfileView: View {
     var body: some View {
         VStack {
             headerView
+            actionButtons
             Spacer()
         }
     }
@@ -42,5 +43,27 @@ extension ProfileView  {
             }
         }
         .frame(height: 96)
+    }
+    
+    var actionButtons: some View {
+        HStack (spacing: 12) {
+            Spacer()
+            Image(systemName: "bell.badge")
+                .font(.title3)
+                .padding(6)
+                .overlay(Circle().stroke(Color.gray, lineWidth: 0.75))
+            
+            Button {
+                
+            } label: {
+                Text("Edit Profile")
+                    .font(.subheadline)
+                    .bold()
+                    .frame(width: 120, height: 32)
+                    .foregroundColor(.black)
+                    .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.gray, lineWidth: 0.75))
+            }
+        }
+        .padding(.trailing)
     }
 }
