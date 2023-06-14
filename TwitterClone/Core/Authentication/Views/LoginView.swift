f8ae9f7f7a2cd68fe01b062c4ea1688c20e6b9b5
tiger <thisisstreamliner@gmail.com>
2023-06-14 20:48:29 +0300

    Create LoginView

---
+++ TwitterClone/Core/Authentication/Views/LoginView.swift
@@ -0,0 +1,93 @@
+//
+//  LoginView.swift
+//  TwitterClone
+//
+//  Created by MEI on 14.06.2023.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+    var body: some View {
+        VStack {
+            VStack (alignment: .leading) {
+                HStack {
+                    Spacer()
+                }
+                Text("Hello.")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                Text("Welcome Back")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+            }
+            .frame(height: 260)
+            .padding(.leading)
+            .background(Color(.systemBlue))
+            .foregroundColor(.white)
+            .clipShape(RoundedShape(corners: [.bottomRight]))
+            
+            VStack (spacing: 40) {
+                CustomInputField(imageName: "envelope", placeholderText: "Email", text: $email)
+                CustomInputField(imageName: "lock", placeholderText: "Password", text: $password)
+            }
+            .padding(.horizontal, 32)
+            .padding(.vertical, 44)
+            
+            HStack {
+                Spacer()
+                
+                NavigationLink {
+                    Text("Reset password view.")
+                } label: {
+                    Text("Forgot Password?")
+                        .font(.caption)
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color(.systemBlue))
+                        .padding(.top)
+                        .padding(.trailing, 24)
+                }
+            }
+            
+            Button {
+                print("sign in here...")
+            } label: {
+                Text("Sign In")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .frame(width: 340, height: 50)
+                    .background(Color(.systemBlue))
+                    .clipShape(Capsule())
+                    .padding()
+            }
+            .shadow(color: .gray.opacity(0.5), radius: 10, x: 0, y: 0)
+            
+            Spacer()
+            
+            NavigationLink {
+                RegistrationView()
+                    .navigationBarHidden(true)
+            } label: {
+                HStack {
+                    Text("Don't have an account?")
+                        .font(.footnote)
+                    Text("Sign Up")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                }
+                .padding(.bottom, 32)
+                .foregroundColor(Color(.systemBlue))
+            }
+        }
+        .ignoresSafeArea(.all)
+        .navigationBarHidden(true)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
---
+++ TwitterClone/Core/Components/CustomInputField.swift
@@ -0,0 +1,35 @@
+//
+//  CustomInputField.swift
+//  TwitterClone
+//
+//  Created by MEI on 14.06.2023.
+//
+
+import SwiftUI
+
+struct CustomInputField: View {
+    let imageName: String
+    let placeholderText: String
+    @Binding var text: String
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Image(systemName: imageName)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 20, height: 20)
+                    .foregroundColor(Color(.darkGray))
+                TextField(placeholderText, text: $text)
+            }
+            Divider()
+                .background(Color(.darkGray))
+        }
+    }
+}
+
+struct CustomInputField_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomInputField(imageName: "envelope", placeholderText: "Email", text: .constant(""))
+    }
+}
